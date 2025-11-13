170b3f6c3cbd87780ad863191cba115c6663cd63
Driver Station <northshorerobotics3950@gmail.com>
2025-11-13 16:56:08 -0500

    yayayya drive work now. reference!

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,7 +40,7 @@ public class RobotContainer {
 
     private final CommandXboxController joystick = new CommandXboxController(0);
 
-    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createSwerveDrivetrain();
+    public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     /* Path follower */
     private final SendableChooser<Command> autoChooser;
