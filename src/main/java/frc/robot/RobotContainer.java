80e58bd9fd8fad34c24c6e36a4e70a18cfe0951f
Driver Station <northshorerobotics3950@gmail.com>
2025-12-09 18:09:47 -0500

    amp command changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -181,10 +181,10 @@ public class RobotContainer {
 
                 // Run SysId routines when holding back/start and X/Y.
                 // Note that each routine should be run exactly once in a single log.
-                joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-                joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-                joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-                joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+                // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+                // joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+                // joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+                // joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
                 // reset the field-centric heading on left bumper press
                 joystick.y().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
@@ -200,15 +200,15 @@ public class RobotContainer {
                 joystick.pov(180).whileTrue(climber.climbCommand(() -> -Constants.climberSpeed));
 
                 // Amp Command
-                Command ampCommand = pivot.ampCommand();
-                joystick.x().whileTrue(ampCommand);
+                
+                joystick.x().whileTrue(pivot.ampCommand());
 
                 // Indexer control: Reverse if Amp command is running, otherwise normal
                 joystick.rightTrigger().whileTrue(
                                 Commands.either(
                                                 intake.indexerReverse(),
                                                 intake.indexer(),
-                                                ampCommand::isScheduled));
+                                                pivot.ampCommand()::isScheduled));
 
                 drivetrain.registerTelemetry(logger::telemeterize);
         }
