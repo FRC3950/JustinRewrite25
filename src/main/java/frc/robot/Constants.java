89b23b1a9805c9b1c25d0de051c31c36ce0a4d82
DriverStation3950 <northshorerobotics3950@gmail.com>
2025-12-15 10:21:34 -0500

    add final constant needed for testing (I think I understand it now). no intaking implemented, just driving.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,5 +40,5 @@ public class Constants {
     public static final String limelightName = "limelight-notes";
     public static final double limelightMountHeight = Units.inchesToMeters(26.5); // Meters
     public static final double limelightMountAngle = 80.0; // Degrees
-    public static final double noteTargetHeight = 0.0; // Meters (on the floor)
+    public static final double noteTargetHeight = Units.inchesToMeters(2); // Meters (on the floor)
 }
